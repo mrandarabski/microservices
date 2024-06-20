abe2b787be842ba520ceb57c66366b70272adbe0
mrandarabski <asad.and@gmail.com>
2024-06-20 10:19:13 +0200

    some changes in yml files

--- v2-spring-cloud-config/configserver/src/main/java/com/lvgod/configserver/ConfigserverApplication.java
+++ v2-spring-cloud-config/configserver/src/main/java/com/lvgod/configserver/ConfigserverApplication.java
@@ -10,6 +10,7 @@ public class ConfigserverApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigserverApplication.class, args);
+        System.out.println("Hi Mr Andarabski the config server is started");
     }
 
 }
